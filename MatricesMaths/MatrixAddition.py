397769b1ded32c571f6098bd58f5b4854aaa45d8
MikeDurrant <mike_durrant@yahoo.co.uk>
2021-03-11 22:20:18 +0000

    with MatrixAddition

---
+++ MatricesMaths/MatrixAddition.py
@@ -0,0 +1,19 @@
+# matrix addition
+
+# component-wise addition
+
+import numpy as np
+
+matrix1 = np.array([[0,1],
+                    [5,4]])
+
+print(matrix1)
+
+matrix2 = np.array([[5,2],
+                    [1,8]])
+
+print(matrix2)
+
+
+matrix3 = matrix1 + matrix2
+print(matrix3)
